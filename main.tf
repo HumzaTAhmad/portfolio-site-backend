8c7c8a2eb46a608b46959f850ea4f63af6132bac
HumzaTAhmad <humza3173@gmail.com>
2024-02-02 17:56:45 -0500

    test

--- main.tf
+++ main.tf
@@ -86,53 +86,7 @@ resource "aws_api_gateway_integration" "MyPortfolioIntegration" {
 }
 
 
-resource "aws_api_gateway_method" "MyPortfolioOptions" {
-  rest_api_id   = aws_api_gateway_rest_api.MyPortfolioAPI.id
-  resource_id   = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
 
-resource "aws_api_gateway_method_response" "MyPortfolioOptionsResponse200" {
-  rest_api_id = aws_api_gateway_rest_api.MyPortfolioAPI.id
-  resource_id = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
-  http_method = aws_api_gateway_method.MyPortfolioOptions.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-
-resource "aws_api_gateway_integration_response" "MyPortfolioOptionsIntegrationResponse" {
-  rest_api_id = aws_api_gateway_rest_api.MyPortfolioAPI.id
-  resource_id = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
-  http_method = aws_api_gateway_method.MyPortfolioOptions.http_method
-  status_code = aws_api_gateway_method_response.MyPortfolioOptionsResponse200.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS'",
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
-}
-
-resource "aws_api_gateway_integration" "MyPortfolioOptionsIntegration" {
-  rest_api_id = aws_api_gateway_rest_api.MyPortfolioAPI.id
-  resource_id = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
-  http_method = aws_api_gateway_method.MyPortfolioOptions.http_method
-
-  type = "MOCK"
-
-  request_templates = {
-    "application/json" = "{ \"statusCode\": 200 }"
-  }
-}
 
 resource "aws_api_gateway_deployment" "my_api_deployment" {
   depends_on = [
