4917a2c2d31c360b38e592e1350e88fc147b1a74
HumzaTAhmad <humza3173@gmail.com>
2024-02-02 17:57:44 -0500

    TEST

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_api_gateway_integration" "MyPortfolioIntegration" {
 resource "aws_api_gateway_deployment" "my_api_deployment" {
   depends_on = [
     aws_api_gateway_integration.MyPortfolioIntegration,
-    aws_api_gateway_integration.MyPortfolioOptionsIntegration
+    #aws_api_gateway_integration.MyPortfolioOptionsIntegration
   ]
   
   rest_api_id = aws_api_gateway_rest_api.MyPortfolioAPI.id
