df8bba88d0bfaf937e19ce1d5c793d0d23d6dbd0
HumzaTAhmad <humza3173@gmail.com>
2024-02-02 19:44:45 -0500

    api gateway integration response correction

--- main.tf
+++ main.tf
@@ -97,14 +97,19 @@ resource "aws_api_gateway_integration" "MyPortfolioIntegration" {
 }
 
 resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+    depends_on = [
+        aws_api_gateway_integration.MyPortfolioIntegration,
+        aws_api_gateway_method_response.response_200
+    ]
+
+
   rest_api_id = aws_api_gateway_rest_api.MyPortfolioAPI.id
   resource_id = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
   http_method = aws_api_gateway_method.MyPortfolioMethod.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
 
   response_templates = {
-    "application/json" = <<EOF
-    EOF
+    "application/json" = ""
   }
 }
 
