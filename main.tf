e1a0006c3769c4a9941078ff75d70c09504e5783
HumzaTAhmad <humza3173@gmail.com>
2024-02-02 21:30:09 -0500

    update

--- main.tf
+++ main.tf
@@ -127,18 +127,59 @@ resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
 }
 
 
-module "cors" {
-  source = "squidfunk/api-gateway-enable-cors/aws"
-  version = "0.3.3"
+resource "aws_api_gateway_method" "MyPortfolioOptions" {
+  rest_api_id   = aws_api_gateway_rest_api.MyPortfolioAPI.id
+  resource_id   = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
 
-  api_id          = aws_api_gateway_rest_api.MyPortfolioAPI.id
-  api_resource_id = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
+resource "aws_api_gateway_method_response" "MyPortfolioOptionsResponse200" {
+  rest_api_id = aws_api_gateway_rest_api.MyPortfolioAPI.id
+  resource_id = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
+  http_method = aws_api_gateway_method.MyPortfolioOptions.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "MyPortfolioOptionsIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.MyPortfolioAPI.id
+  resource_id = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
+  http_method = aws_api_gateway_method.MyPortfolioOptions.http_method
+  status_code = aws_api_gateway_method_response.MyPortfolioOptionsResponse200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
+resource "aws_api_gateway_integration" "MyPortfolioOptionsIntegration" {
+  rest_api_id = aws_api_gateway_rest_api.MyPortfolioAPI.id
+  resource_id = aws_api_gateway_rest_api.MyPortfolioAPI.root_resource_id
+  http_method = aws_api_gateway_method.MyPortfolioOptions.http_method
+
+  type = "MOCK"
+
+  request_templates = {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
 }
 
 
 resource "aws_api_gateway_deployment" "my_api_deployment" {
   depends_on = [
     aws_api_gateway_integration.MyPortfolioIntegration,
+    aws_api_gateway_integration.MyPortfolioOptionsIntegration
   ]
   
   rest_api_id = aws_api_gateway_rest_api.MyPortfolioAPI.id
